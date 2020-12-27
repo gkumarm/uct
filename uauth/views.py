faa3a643924fb3c1d5265f0d0471ef37144687c1
Gireesh Kumar <gk.malattiri@gmail.com>
2020-12-27 23:40:25 +0400

    Changes to replace login page with modernized login page

--- uauth/views.py
+++ uauth/views.py
@@ -73,6 +73,6 @@ def user_login(request):
 		else:
 			print("Someone tried to login and failed.")
 			print("They used username: {} and password: {}".format(username,password))
-			return HttpResponse("Invalid login details given")
+			return render(request, 'uauth/login.html', {'errmsg':'Invalid login details given'})
 	else:
 		return render(request, 'uauth/login.html', {})
