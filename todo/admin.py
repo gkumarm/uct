14932c0b4747551f11607d425f4493c1f0e2abb2
Gireesh Kumar <gk.malattiri@gmail.com>
2021-01-28 14:59:25 +0400

    Published TodoTypes and Context for Admin CRUD

--- todo/admin.py
+++ todo/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Todo, Resource
+from .models import Context, TodoType
 
-admin.site.register (Todo)
-admin.site.register (Resource)
+admin.site.register (Context)
+admin.site.register (TodoType)
+
+admin.site.site_header = 'UCT TODO'
+admin.site.site_title = 'UCT'
