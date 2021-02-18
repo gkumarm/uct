8869219f2ef8ca1c9d6a8984d354c1c25f3dd047
Gireesh Kumar <gk.malattiri@gmail.com>
2021-02-18 23:30:45 +0400

    TodoStatus added in the admin site

--- todo/admin.py
+++ todo/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Context, TodoType
+from .models import Context, TodoType, TodoStatus
 
 admin.site.register (Context)
 admin.site.register (TodoType)
+admin.site.register (TodoStatus)
 
 admin.site.site_header = 'UCT TODO'
 admin.site.site_title = 'UCT'
